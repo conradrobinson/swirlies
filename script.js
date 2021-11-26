8d18731b4bc29f349a5af8295f890f33c4b76d2b
conradrobinson <58628971+conradrobinson@users.noreply.github.com>
2021-11-26 22:40:23 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,87 @@
+class Arc {
+    constructor(num, total) {
+        this.num = num
+        this.total = total
+    }
+    generate(bias1, bias2) {
+        this.radius = random(10, (Math.max(dims[0], dims[1])/2)+50)
+        let awayFromCenterBias = 1;
+        if (bias1){
+            awayFromCenterBias =  Math.max(Math.sin((Math.PI / 180) * ((this.radius/(Math.max(dims[0], dims[1])/2))*90))*1.3, 0.7) //sin curve of value from center and caps
+        //at a min of 0.4 so 40% of the way out
+        }
+        let firstElementsAreLargestBias = 1
+        if (bias2) {
+         firstElementsAreLargestBias = Math.sin((Math.PI / 180) * ((this.total-this.num)/this.total) * 90) * 8
+        }
+        this.weight = firstElementsAreLargestBias * awayFromCenterBias * random(5, 30)
+        this.rads = random(0, 1/2 * 2 * Math.PI)
+        this.start = random(0, 2*Math.PI - this.rads)
+        this.speed = (random(0.1, 2)/50)
+        this.biasspeed = Math.min((random(0.1, 2)/50)*(1/firstElementsAreLargestBias), this.speed)
+        this.colour = hslToHex(random(230, 285), random(80, 100), random(0, Math.min(firstElementsAreLargestBias*60, 60)))
+        //basically the first elements to be added to the array (the behind ones) are the largest, darkest, and slowest - the firstElementsAreLargestBias
+        //and the elements closest to the center are the smallest - the awayFromCenterBias
+        //this sums up to the first elements to be added to the outside are the largest, slowest and darkest, and the middle ones tend towards being smaller
+        //everything with a heavy degree of randomness with parameters like (5, 30) for weight as an example. That's nearly all random.
+    }
+    display(canvas) {
+        canvas.beginPath()
+        canvas.lineWidth = this.weight
+        canvas.strokeStyle = this.colour
+        canvas.arc(center[0], center[1], this.radius, this.start, this.rads+this.start, false)
+        canvas.stroke()
+    }
+    move() {
+        this.start += this.biasspeed
+    }
+}
+
+
+dims = [window.innerWidth, window.innerHeight]
+center = [dims[0]/2, dims[1]/2]
+c = document.getElementById("cnvs");
+c.width = window.innerWidth;
+c.height = window.innerHeight;
+canvas = c.getContext("2d")
+canvas.lineCap = "round"
+
+let AWAYFROMCENTERBIAS = true
+let FIRSTDRAWNBIAS = true
+
+
+arcs = []
+let a = 200
+for (let i = 0; i < a; i++) {
+    arcs.push(new Arc(i, a));
+}
+for (let i = 0; i < arcs.length; i++) {
+    arcs[i].generate(AWAYFROMCENTERBIAS, FIRSTDRAWNBIAS);
+    arcs[i].display(canvas)
+
+}
+setInterval(function() {
+    canvas.clearRect(0, 0, c.width, c.height)
+    for (let j = 0; j < arcs.length; j++) {
+        arcs[j].display(canvas)
+        arcs[j].move();
+    }  
+}, 10)
+
+
+
+
+function random (min, max) {
+    return (Math.random() * (max-min)) + min
+}
+
+function hslToHex(h, s, l) {
+    l /= 100;
+    const a = s * Math.min(l, 1 - l) / 100;
+    const f = n => {
+      const k = (n + h / 30) % 12;
+      const color = l - a * Math.max(Math.min(k - 3, 9 - k, 1), -1);
+      return Math.round(255 * color).toString(16).padStart(2, '0');   // convert to Hex and prefix "0" if needed
+    };
+    return `#${f(0)}${f(8)}${f(4)}`;
+  }
